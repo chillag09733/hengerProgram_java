6b2315cdc4483924f1c5ba70345be92c93ffdada
chillag09733 <chillag09733@gmail.com>
2025-01-05 18:29:51 +0100

    finished

--- src/hengerprogram/TomorHenger.java
+++ src/hengerprogram/TomorHenger.java
@@ -6,7 +6,7 @@ public class TomorHenger extends Henger {
 
     public TomorHenger(double sugar, double magassag, double f) {
         //meghívjuk a henger osztály konstruktorát, nem kell újra implementálni a szülőosztályban megírt kódot
-        //átadjuk a sugar és magassag paramétereket, hogy inicializálja a szülőosztályban található mezőket
+        //átadjuk a sugar és magassag paramétereket, hogy inicializálja a szülőosztályban található értékeket
         super(sugar, magassag);
         this.fajsuly = f;
     }
